f92228f037c13c450423c49f68b1d0d4929edc3c
기범석 <kbs9406@gmail.com>
2023-08-17 09:40:25 +0900

    포트폴리오 샘플

---
+++ js/profile.js
@@ -0,0 +1,43 @@
+axios.get("../data/data.json")
+.then(function(res){
+    console.log(res);
+    res.data.Profile.map((e,i)=>{
+        console.log(e)
+
+        let div = document.createElement("div");
+        div.className = "bg-white rounded-md border dark:bg-[#272929] dark:text-[#ebf4f1] p-5 mb-8 last:mb-0";
+
+        let h3 = document.createElement("h3");
+        h3.className = "text-base md:text-xl mb-4"
+        h3.textContent = e.title;
+
+        div.appendChild(h3)
+        
+        let p;
+// 조건문에 "아닌 경우" 는 느낌표를 사용
+        if(e.desc){
+            p= document.createElement("p");
+            p.innerHTML = `<class="font-bold">'${e.desc}'</span> ${e.desc2}`
+            p.className ="mb-2 last:mb-0 text-sm md:text-base"
+            div.appendChild(p)
+            
+           
+        }else{
+            e.date.map((el,index)=>{
+                console.log(el)
+                p= document.createElement("p");
+                p.className ="mb-2 last:mb-0 text-sm md:text-base"
+                p.textContent = `$(el) : ${e.dateDesc[index]}`;
+                // 반복문 안에 반복문을 사용할땐 이렇게도 사용할 수 있다.
+                div.appendChild(p)
+            })
+
+        }
+        
+
+        document.querySelector(".profile-content").appendChild(div)
+    })
+})
+.catch(function(error){
+    console.log(error);
+})
